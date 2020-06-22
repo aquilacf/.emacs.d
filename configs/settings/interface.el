5e48b571bea5e4494c243cd286fbedd583b512e1
Áquila Freitas <aquilacf@protonmail.com>
2020-06-22 12:16:03 +0100

    Improvements to tabing.

--- configs/settings/interface.el
+++ configs/settings/interface.el
@@ -3,9 +3,9 @@
 ;;; Code:
 
 ;; Interface
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode nil))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode nil))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode nil))
 (transient-mark-mode t)
 (xterm-mouse-mode t) ; enable mouse
 
@@ -40,6 +40,15 @@
 ;; Change all prompts to y or n
 (fset 'yes-or-no-p 'y-or-n-p)
 
+												; Defaults for identation/trailing:
+(setq-default 	delete-trailing-lines t			; 	Delete useless empty lines at bottom.
+				delete-trailing-whitespace t	; 	Delete useless whitespaces.
+				indent-tabs-mode t				;	Tabs, not spaces.
+				tab-always-indent nil			;	Force tab insertion. @todo add shit-tab for 'compete
+				tab-width 4)					;	Tab with.
+												; Editorconfig can override this.
 
-(setq delete-trailing-lines nil) ; Prevent from deleting EOF line
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(provide 'interface)
+
+;;; interface.el ends here
